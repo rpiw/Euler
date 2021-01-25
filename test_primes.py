e987b9a344886930f305b49621bd57accf8f1ffd
rpiw <radoslawp95@gmail.com>
2021-01-25 22:41:24 +0100

    Minor changes, formatting

--- test_primes.py
+++ test_primes.py
@@ -1,15 +1,10 @@
-from unittest import TestCase, expectedFailure
+from unittest import TestCase
 import numpy as np
 import primes
 
 
 class Test(TestCase):
     primes = np.loadtxt("primes_correct.txt", dtype=int).flatten()
-
-    def test_sieve_of_atkin(self):
-        upper_limit = 15485863 + 1
-        for correct, calculated in zip(Test.primes, primes.sieve_of_atkin(upper_limit)):
-            self.assertEqual(correct, calculated, msg=f"{correct}, {calculated}")
 
     def test_sieve_of_sundaram(self):
         upper_limit = 15485863 + 1
